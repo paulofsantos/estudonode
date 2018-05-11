09b470552842fd00e77eec18b767732041fdcac5
Paulo Faria <paulinhum.faria@hotmail.com>
2018-05-10 22:55:23 -0300

    Remover console.log

--- server.js
+++ server.js
@@ -213,4 +213,3 @@ app.use('/api', apiRoutes);//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~para que server o coma
 
 app.listen(port);
 console.log('Magic Happens at http://localhost:'+ port);
-console.log('Test');
